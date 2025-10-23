42678c69d596d3f5a8c0a6f6d7aa11155130b6e5
Alex <bloschitzyn@yandex.ru>
2025-10-23 19:29:05 +0300

    для озвучки

--- classes.py
+++ classes.py
@@ -23,3 +23,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#https://www.youtube.com/watch?v=R59bwGBMn7s&t=522s
+#https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c
+#https://github.com/suno-ai/bark?tab=readme-ov-file
+#https://www.youtube.com/watch?v=UgHWlE7gQHI
