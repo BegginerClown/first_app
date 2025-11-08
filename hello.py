2050d216909ee91587ee4a799bc9530d7e48d5eb
Olexey <bloschitzyn@yandex.ru>
2025-11-08 13:32:14 +0300

    Движение в сторону игры

--- hello.py
+++ hello.py
@@ -27,6 +27,12 @@ class Player():
         self.walk_left_frames = [
             pygame.image.load(f'images/player/walk left/{i}.png') for i in range(1, 7)
         ]
+        self.walk_back_frames = [
+            pygame.image.load(f'images/player/walk back/{i}.png') for i in range(1, 3)
+        ]
+        self.walk_front_frames = [
+            pygame.image.load(f'images/player/walk front/{i}.png') for i in range(1, 3)
+        ]
         self.chill_frames = [
             pygame.image.load('images/player/chill right/1.png'),
             pygame.image.load('images/player/chill left/1.png')
@@ -57,13 +63,27 @@ class Player():
     def moving(self, keys):
         self.update_animation()
         if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.x > 200:
+            self.speed = 4
+            self.animation_speed = 8
             self.x -= self.speed
             self.current_frames = self.walk_left_frames
             self.last_direction = 'left'
         elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.x < 1100:
+            self.speed = 4
+            self.animation_speed = 8
             self.x += self.speed
             self.current_frames = self.walk_right_frames
             self.last_direction = 'right'
+        elif (keys[pygame.K_UP] or keys[pygame.K_w]) and self.y >= 400:
+            self.speed = 2
+            self.animation_speed = 12
+            self.y -= self.speed
+            self.current_frames = self.walk_back_frames
+        elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.y <= 500:
+            self.speed = 2
+            self.animation_speed = 12
+            self.y += self.speed
+            self.current_frames = self.walk_front_frames
         else:
             if self.last_direction == 'right':
                 self.current_frame = 0
@@ -72,7 +92,21 @@ class Player():
             self.current_frames = self.chill_frames
 
     def draw(self, surface):
-        surface.blit(self.current_frames[self.current_frame], (self.x, self.y))
+        current_sprite = self.current_frames[self.current_frame]
+        original_width = current_sprite.get_width()
+        original_height = current_sprite.get_height()
+        scale_factor = max(0.5, 1.0 - (500 - self.y) / 500)
+        scaled_sprite = pygame.transform.scale(
+            current_sprite,
+            (int(current_sprite.get_width() * scale_factor),
+             int(current_sprite.get_height() * scale_factor))
+        )
+        scaled_width = scaled_sprite.get_width()
+        scaled_height = scaled_sprite.get_height()
+
+        offset_x = self.x - (scaled_width - original_width) // 2
+        offset_y = self.y - (scaled_height - original_height) // 2
+        surface.blit(scaled_sprite, (offset_x, offset_y))
 
 
 player = Player(name='Pete')
