950683d267d7ef85127cc0be6a17389d63f111bc
Olexey <bloschitzyn@yandex.ru>
2025-11-08 14:40:06 +0300

    Разделение на файлы

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import pygame
+from player_core.player import Player
 
 clock = pygame.time.Clock()
 
@@ -10,109 +11,7 @@ icon = pygame.image.load('images/icons/icon.png')
 bg = pygame.image.load('images/back/back.png')
 pygame.display.set_icon(icon)
 
-class Player():
-    def __init__(self, name: str, x=200, y=500):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.walk_speed = 4
-        self.back_front_speed = 2
-        self.current_frame = 0
-        self.animation_speed = 8
-        self.back_front_anim_speed = 12
-        self.frame_counter = 0
-        self.last_direction = 'right'
-
-        self.walk_right_frames = [
-            pygame.image.load(f'images/player/walk right/{i}.png') for i in range(1, 7)
-        ]
-        self.walk_left_frames = [
-            pygame.image.load(f'images/player/walk left/{i}.png') for i in range(1, 7)
-        ]
-        self.walk_back_frames = [
-            pygame.image.load(f'images/player/walk back/{i}.png') for i in range(1, 3)
-        ]
-        self.walk_front_frames = [
-            pygame.image.load(f'images/player/walk front/{i}.png') for i in range(1, 3)
-        ]
-        self.chill_frames = [
-            pygame.image.load('images/player/chill right/1.png'),
-            pygame.image.load('images/player/chill left/1.png')
-        ]
-
-        self.direction = 'right'
-        self.current_frames = self.walk_right_frames
-
-    def update_animation(self, anim_speed):
-        self.frame_counter += 1
-        if self.frame_counter >= anim_speed:
-            self.current_frame = (self.current_frame + 1) % len(self.current_frames)
-            self.frame_counter = 0
-
-    def self_moving(self):
-        self.update_animation(self.animation_speed)
-        if self.direction == 'right':
-            self.x += self.walk_speed
-            if self.x >= 1100:
-                self.direction = 'left'
-                self.current_frames = self.walk_left_frames
-        elif self.direction == 'left':
-            self.x -= self.walk_speed
-            if self.x <= 200:
-                self.direction = 'right'
-                self.current_frames = self.walk_right_frames
-
-    def moving(self, keys):
-        old_frames = self.current_frames
-        if (keys[pygame.K_UP] or keys[pygame.K_w]) and self.y >= 400:
-            self.update_animation(self.back_front_anim_speed)
-            self.y -= self.back_front_speed
-            self.current_frames = self.walk_back_frames
-        elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.y <= 500:
-            self.update_animation(self.back_front_anim_speed)
-            self.y += self.back_front_speed
-            self.current_frames = self.walk_front_frames
-        elif (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.x > 200:
-            self.update_animation(self.animation_speed)
-            self.x -= self.walk_speed
-            self.current_frames = self.walk_left_frames
-            self.last_direction = 'left'
-        elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.x < 1100:
-            self.update_animation(self.animation_speed)
-            self.x += self.walk_speed
-            self.current_frames = self.walk_right_frames
-            self.last_direction = 'right'
-        else:
-            if self.last_direction == 'right':
-                self.current_frame = 0
-            elif self.last_direction == 'left':
-                self.current_frame = 1
-            self.current_frames = self.chill_frames
-        if self.current_frames != old_frames:
-            self.current_frame = 0
-
-    def draw(self, surface):
-        current_sprite = self.current_frames[self.current_frame]
-        original_width = current_sprite.get_width()
-        original_height = current_sprite.get_height()
-        scale_factor = max(0.5, 1.0 - (500 - self.y) / 500)
-        scaled_sprite = pygame.transform.scale(
-            current_sprite,
-            (int(current_sprite.get_width() * scale_factor),
-             int(current_sprite.get_height() * scale_factor))
-        )
-        scaled_width = scaled_sprite.get_width()
-        scaled_height = scaled_sprite.get_height()
-
-        offset_x = self.x - (scaled_width - original_width) // 2
-        offset_y = self.y - (scaled_height - original_height) // 2
-        surface.blit(scaled_sprite, (offset_x, offset_y))
-
-
 player = Player(name='Pete')
-
-
-
 
 
 running = True
@@ -132,7 +31,7 @@ while running:
     if debug_mode:
         text = font.render(f'X: {player.x}, Y: {player.y}', True, (255, 255, 255))
         screen.blit(text, (10, 10))
-    player.draw(screen)
+    player.draw(screen, screen.get_width())
     pygame.display.update()
 
     clock.tick(60)
---
+++ player_core/player.py
@@ -0,0 +1,116 @@
+import pygame
+class Player():
+    def __init__(self, name: str, x=200, y=500):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.walk_speed = 4
+        self.back_front_speed = 2
+        self.current_frame = 0
+        self.animation_speed = 8
+        self.back_front_anim_speed = 12
+        self.frame_counter = 0
+        self.last_direction = 'right'
+
+        self.walk_right_frames = [
+            pygame.image.load(f'images/player/walk right/{i}.png') for i in range(1, 7)
+        ]
+        self.walk_left_frames = [
+            pygame.image.load(f'images/player/walk left/{i}.png') for i in range(1, 7)
+        ]
+        self.walk_back_frames = [
+            pygame.image.load(f'images/player/walk back/{i}.png') for i in range(1, 3)
+        ]
+        self.walk_front_frames = [
+            pygame.image.load(f'images/player/walk front/{i}.png') for i in range(1, 3)
+        ]
+        self.chill_frames = [
+            pygame.image.load('images/player/chill right/1.png'),
+            pygame.image.load('images/player/chill left/1.png')
+        ]
+
+        self.direction = 'right'
+        self.current_frames = self.walk_right_frames
+
+    def update_animation(self, anim_speed):
+        self.frame_counter += 1
+        if self.frame_counter >= anim_speed:
+            self.current_frame = (self.current_frame + 1) % len(self.current_frames)
+            self.frame_counter = 0
+
+    def self_moving(self):
+        self.update_animation(self.animation_speed)
+        if self.direction == 'right':
+            self.x += self.walk_speed
+            if self.x >= 1100:
+                self.direction = 'left'
+                self.current_frames = self.walk_left_frames
+
+        elif self.direction == 'left':
+            self.x -= self.walk_speed
+            if self.x <= 200:
+                self.direction = 'right'
+                self.current_frames = self.walk_right_frames
+
+    def moving(self, keys):
+        old_frames = self.current_frames
+
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and self.y >= 400:
+            self.update_animation(self.back_front_anim_speed)
+            self.y -= self.back_front_speed
+            self.current_frames = self.walk_back_frames
+
+        elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.y <= 500:
+            self.update_animation(self.back_front_anim_speed)
+            self.y += self.back_front_speed
+            self.current_frames = self.walk_front_frames
+
+        elif (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.x > 200:
+            self.update_animation(self.animation_speed)
+            self.x -= self.walk_speed
+            self.current_frames = self.walk_left_frames
+            self.last_direction = 'left'
+
+        elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.x < 1100:
+            self.update_animation(self.animation_speed)
+            self.x += self.walk_speed
+            self.current_frames = self.walk_right_frames
+            self.last_direction = 'right'
+
+        else:
+            if self.last_direction == 'right':
+                self.current_frame = 0
+            elif self.last_direction == 'left':
+                self.current_frame = 1
+            self.current_frames = self.chill_frames
+
+        if self.current_frames != old_frames:
+            self.current_frame = 0
+
+    def draw(self, surface, screen_width):
+        current_sprite = self.current_frames[self.current_frame]
+        original_width = current_sprite.get_width()
+        original_height = current_sprite.get_height()
+
+        scale_factor = max(0.5, 1.0 - (500 - self.y) / 500)
+        scaled_sprite = pygame.transform.scale(
+            current_sprite,
+            (int(current_sprite.get_width() * scale_factor),
+             int(current_sprite.get_height() * scale_factor))
+        )
+
+        scaled_width = scaled_sprite.get_width()
+        scaled_height = scaled_sprite.get_height()
+        offset_y = self.y - (scaled_height - original_height) // 2
+
+        center_x = screen_width // 2
+        offset_x = self.x - (scaled_width - original_width) // 2
+
+        perspective_factor = (500 - self.y) /500
+        perspective_factor = min(1.0, perspective_factor * 3.0)
+        distance_from_center = abs(offset_x - center_x)
+        max_distance = center_x
+        adjusted_perspective = perspective_factor * (1 - distance_from_center / max_distance)
+        offset_x = offset_x * (1 - adjusted_perspective) + center_x * adjusted_perspective
+
+        surface.blit(scaled_sprite, (offset_x, offset_y))
